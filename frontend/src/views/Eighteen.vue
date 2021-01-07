0bb3a260d10ce47568d2af0769be4385b46f1a8c
Psycarlo <karkosyk@gmail.com>
2021-01-07 18:46:37 +0000

    :arrow_right: Progress

---
+++ frontend/src/views/Eighteen.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <h1>Are you 18+?</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script></script>
