50c78cb64812bb9933719c1df1ad01f05b901366
Psycarlo <karkosyk@gmail.com>
2021-01-10 00:24:07 +0000

    :white_square_button: Eighteen buttons

--- frontend/src/views/Eighteen.vue
+++ frontend/src/views/Eighteen.vue
@@ -1,7 +1,20 @@
 <template>
   <v-container fill-height fluid>
     <v-row align="center" justify="center">
-      <h1 class="primary--text">{{ $t("questions.eighteen") }}</h1>
+      <h1 class="primary--text text-center">{{ $t("questions.eighteen") }}</h1>
+      <v-col cols="12" class="mt-8 text-center">
+        <!-- TODO: Below 300px the buttons have no margin top/bottom and are not align center -->
+        <v-btn
+          class="mr-4 primary--text"
+          color="primary"
+          outlined
+          min-width="120"
+          >{{ $t("answers.no") }}</v-btn
+        >
+        <v-btn class="ml-4 secondary--text" color="primary" min-width="120">{{
+          $t("answers.yes")
+        }}</v-btn>
+      </v-col>
     </v-row>
   </v-container>
 </template>
